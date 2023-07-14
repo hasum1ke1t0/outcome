356292ef926f843d6328e7852081520ec5b6dbcd
hasm1ke1t0 <hachi.horiuchi@gmail.com>
2023-07-14 10:42:22 +0900

    修正

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Item;
+class ItemController extends Controller
+{
+    public function index(Item $item)//インポートしたPostをインスタンス化して$postとして使用。
+    {
+        return $item->get();//$postの中身を戻り値にする。
+    }
+}
