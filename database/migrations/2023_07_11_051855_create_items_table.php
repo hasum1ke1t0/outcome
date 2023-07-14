5b6c5dc5759a63a3ce9f1d26150a4b6164b47f9f
hasm1ke1t0 <hachi.horiuchi@gmail.com>
2023-07-14 10:25:29 +0900

    適切なコミットコメント

--- database/migrations/2023_07_11_051855_create_items_table.php
+++ database/migrations/2023_07_11_051855_create_items_table.php
@@ -13,15 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('items', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('school', 30);
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('course', 50);
             $table->timestamps();
+            $table->softDeletes();
+
         });
     }
 
@@ -32,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('items');
     }
 };
